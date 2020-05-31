d7cc517a7687fffcb08fdbdea23857bf72e94d83
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2020-06-01 06:38:02 +0700

    One line added

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
 #bot.py
 import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD TOKEN')
+
+client = discord.client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+client.run(TOKEN)
+
 
