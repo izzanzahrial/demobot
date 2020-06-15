c058ab6503292b8a5478aeb840823c454fbf5632
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2020-06-16 06:25:51 +0700

    One line added

--- bot.py
+++ bot.py
@@ -1,147 +1,63 @@
 import discord
-import os
-import json
-import youtube_dl
-from discord.ext import commands, tasks
-from itertools import cycle
+import random
+from discord.ext import commands
 
-#prefix
-def get_prefix(client, message):
-    with open('prefixes.json','r') as f:
-        prefixes = json.load(f)
-
-    return prefixes[str(message.guild.id)]
-
-client = commands.Bot(command_prefix = get_prefix)
-
-#bot status
-status = cycle(['Status 1','Status 2'])
-
-@tasks.loop(seconds=10)
-async def change_status():
-    await client.change_presence(activity=discord.Game(next(status)))
+client = commands.Bot(command_prefix='-')
 
 @client.event
 async def on_ready():
-    change_status.start()
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Listening to your hearth <3'))
-    print('bot is ready!!!')
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_guild_join(guild):
-    with open('prefixes.json','r') as f:
-        prefixes = json.load(f)
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    prefixes[str(guild.id)] = '-'
-
-    with open('prefixes.json','w') as f:
-        json.dump(prefixes, f, indent=4)
-
-@client.event
-async def on_guild_remove(guild):
-    with open('prefixes.json','r') as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-
-    with open('prefixes.json','w') as f:
-        json.dump(prefixes, f, indent=4)
+    if message.content.startswith('-hello'):
+        await message.channel.send('Hello!')
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server.')
+    for channel in member.guild.channels:
+        if str(channel) == "general":
+            print(f'{member} has joined the server')
+            await member.create_dm()
+            await member.dm_channel.send(f'Hi {member.name}, welcome to the server!', delete_after = 50)
+            await channel.send(f'Hi {member.mention}, yang betah di server ya!')
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server.')
-
-@client.command
-async def change_prefix(ctx, prefix):
-    with open('prefixes.json','r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json','w') as f:
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f'Prefixes change to {prefix}')
-
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please pass all required arguments.')
-
-#doesnt work
-@client.command()
-async def clear(ctx, amount = 5):
-    await ctx.channel.purge(limit=amount+1)
-
-@clear.error
-async def clear_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please specify an amount of message to delete.')
-
-#Administrator
-@client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-    await member.kick(reason=reason)
+    for channel in member.guild.channels:
+        if str(channel) == "general":
+            print(f'{member} has left the server')
+            await channel.send(f'Bye bye {member.mention}!')
 
 @client.command()
-async def ban(ctx, member : discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}')
+async def ping(ctx):
+    await ctx.send(f'Pong {round(client.latency*1000)}ms')
 
-@client.command()
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
+@client.command(aliases = ['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain',
+                 'It is decidedly so',
+                 'Without a doubt',
+                 'Yes – definitely',
+                 'You may rely on it',
+                 'As I see it, yes',
+                 'Most likely',
+                 'Outlook good',
+                 'Yes',
+                 'Signs point to yes',
+                 'Reply hazy, try again',
+                 'Ask again later',
+                 'Better not tell you now',
+                 'Cannot predict now',
+                 'Concentrate and ask again',
+                 "Don't count on it",
+                 'My reply is no',
+                 'My sources say no',
+                 'Outlook not so good',
+                 'Very doubtful']
+    await ctx.send(f'Question : {question}\nAnswer : {random.choice(responses)}')
 
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}')
-            return
-
-#Cogs
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-@client.command()
-async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')    
-    client.load_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
-players = {}
-
-@client.command()
-async def join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
-
-@client.command()
-async def leave(ctx):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    await voice_client.disconnect()
-
-@client.command()
-async def play(ctx,url):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    player = await voice_client.create_ytbdl_player(url)
-    players[server.id] = player
-    player.start()
-
-client.run('NzE2MDgzOTY2NjM3MjQ0NTE2.XtRsJg.yLPvDdlvXvvi_g8OuPqygScctSg')
+client.run('NzE2MDgzOTY2NjM3MjQ0NTE2.XucS1A.oQhgb6f3D2Y8essxtH3jxLHhB-Y')
