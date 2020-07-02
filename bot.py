3664b3186b09e92127f5dacf31e939950ea98c4c
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2020-07-02 20:07:16 +0700

    One line added

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
 import discord
-import random
+import os
+import youtube_dl
 from discord.ext import commands
+from discord.ext import tasks
+from discord.utils import get
 
 client = commands.Bot(command_prefix='-')
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Activity(type=discord.ActivityType.listening, name='-help'))
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('-hello'):
-        await message.channel.send('Hello!')
+@tasks.loop(seconds=5)
+async def loop_status():
+    pass #https://www.youtube.com/watch?v=RK8RzuUMYt8&list=PLW3GfRiBCHOhfVoiDZpSz8SM_HybXRPzZ&index=9
 
 @client.event
 async def on_member_join(member):
@@ -34,30 +35,118 @@ async def on_member_remove(member):
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong {round(client.latency*1000)}ms')
+    await ctx.send(f'Pong {round(client.latency * 1000)}ms')
 
-@client.command(aliases = ['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ['It is certain',
-                 'It is decidedly so',
-                 'Without a doubt',
-                 'Yes – definitely',
-                 'You may rely on it',
-                 'As I see it, yes',
-                 'Most likely',
-                 'Outlook good',
-                 'Yes',
-                 'Signs point to yes',
-                 'Reply hazy, try again',
-                 'Ask again later',
-                 'Better not tell you now',
-                 'Cannot predict now',
-                 'Concentrate and ask again',
-                 "Don't count on it",
-                 'My reply is no',
-                 'My sources say no',
-                 'Outlook not so good',
-                 'Very doubtful']
-    await ctx.send(f'Question : {question}\nAnswer : {random.choice(responses)}')
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Invalid command')
 
-client.run('NzE2MDgzOTY2NjM3MjQ0NTE2.XucS1A.oQhgb6f3D2Y8essxtH3jxLHhB-Y')
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount = 10):
+    await ctx.channel.purge(limit=amount+1)
+
+def is_it_me(ctx):
+    return ctx.author.id == 327723419443658753
+
+@client.command()
+@commands.check(is_it_me)
+async def onlyme(ctx):
+    await ctx.send(f'Only you {ctx.author.mention} no one can')
+
+@clear.error
+async def clear_error(ctx,error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please pass in all required arguments.')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+
+@client.command(aliases=['j'])
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"Joined {channel}")
+
+@client.command(aliases=['L','l'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f'Bot has left the {channel}')
+        await ctx.send(f'Bot left the {channel}')
+    else:
+        print(f"Bot wans't not in the {channel}")
+        await ctx.send(f"I'am not in the {channel}")
+
+@client.command(aliases=['p'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("The song has been removed")
+    except PermissionError:
+        print("Can't remove song, cause being played")
+        await ctx.send("ERROR: Music is playing")
+        return
+
+    await ctx.send("Getting ready")
+
+    voice = get(client.voice_clients,guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessor' : [{
+            'key' : 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading song now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
+
+client.run(YOUR_TOKEN)
